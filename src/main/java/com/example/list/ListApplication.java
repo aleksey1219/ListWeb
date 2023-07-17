c24e08486799b7f09beac3659fe9daf1dda2c9b1
1 <glotov-aleks@mail.ru>
2023-07-17 18:46:15 +0400

    list

---
+++ src/main/java/com/example/list/ListApplication.java
@@ -0,0 +1,14 @@
+package com.example.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListApplication.class, args);
+    }
+
+}
